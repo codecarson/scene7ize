befded7039d53ee3a96ab98b2fd56fe93e9311e0
Brad Carson <brad@pixelwavedesign.com>
2012-10-23 11:46:14 -0400

    width and height test cases

--- spec/scene7izer_spec.rb
+++ spec/scene7izer_spec.rb
@@ -8,7 +8,7 @@ describe Scene7izer do
   it "should display logging info if verbose mode on"
   it "should open image files relative to the input file"
 
-  context "scene7 path" do
+  context "scene7 url" do
     it "should contain a specified prefix"
     it "should create a valid scene7 url" do
       image = double("Image", :width => 10, :height => 15, :format => 'jpg')
@@ -17,5 +17,22 @@ describe Scene7izer do
       scene7url = Scene7izer.scene7url_from("http://example.com/scene7/", "test.jpg")
       scene7url.should == "http://example.com/scene7/test?wid=10&hei=15&qlt=100"
     end
+
+    it "should contain a valid width parameter" do
+      image = double("Image", :width => 10, :height => 15, :format => 'jpg')
+      MiniMagick::Image.should_receive(:open).and_return(image)
+
+      scene7url = Scene7izer.scene7url_from("http://example.com/scene7/", "test.jpg")
+      scene7url.should match /wid=10/
+    end
+
+    it "should contain a valid height parameter" do
+      image = double("Image", :width => 10, :height => 15, :format => 'jpg')
+      MiniMagick::Image.should_receive(:open).and_return(image)
+
+      scene7url = Scene7izer.scene7url_from("http://example.com/scene7/", "test.jpg")
+      scene7url.should match /hei=15/
+    end
+
   end
 end
